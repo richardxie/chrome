d9ed8f77da3a46563b22294f9cdc65361578f799
richard.xieq <xieqiang@shanlinjinrong.com>
2017-08-15 13:56:00 +0800

    雅堂chrome扩展程序
    chrome扩展程序，用于投标

---
+++ ytExtension/background.js
@@ -0,0 +1,47 @@
+﻿function getDomainFromUrl(url){
+	var host = "null";
+	if(typeof url == "undefined" || null == url)
+		url = window.location.href;
+	var regex = /.*\:\/\/([^\/]*).*/;
+	var match = url.match(regex);
+	if(typeof match != "undefined" && null != match)
+		host = match[1];
+	return host;
+}
+
+function checkForValidUrl(tabId, changeInfo, tab) {
+	if(getDomainFromUrl(tab.url).toLowerCase()=="jr.yatang.cn"){
+		chrome.pageAction.show(tabId);
+	}
+};
+
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+chrome.tabs.getCurrent(function(tab){  
+ console.log(tab);    
+ });
+var articleData = {};
+articleData.error = "加载中...";
+chrome.runtime.onMessage.addListener(function(request, sender, sendRequest){
+	if(request.type!=="cnblog-article-information")
+		console.log("无效的消息类型：%s", request.type);
+		return;
+	articleData = request;
+	articleData.firstAccess = "获取中...";
+	if(!articleData.error){
+		$.ajax({
+			url: "http://localhost/first_access.php",
+			cache: false,
+			type: "POST",
+			data: JSON.stringify({url:articleData.url}),
+			dataType: "json"
+		}).done(function(msg) {
+			if(msg.error){
+				articleData.firstAccess = msg.error;
+			} else {
+				articleData.firstAccess = msg.firstAccess;
+			}
+		}).fail(function(jqXHR, textStatus) {
+			articleData.firstAccess = textStatus;
+		});
+	}
+});
